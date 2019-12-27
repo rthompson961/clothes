3e5bb55886b0f2310e13dad9d2a7fb13161c60a9
richard <richard@email.com>
2019-12-27 19:29:23 +0000

    Added login system

---
+++ tests/Controller/SecurityControllerTest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class SecurityControllerTest extends WebTestCase
+{
+    private $client;
+
+    protected function setUp()
+    {
+        $this->client = static::createClient();
+    }
+
+    public function testResponse()
+    {
+       $this->client->request('GET', '/login');
+       $this->assertResponseIsSuccessful();
+    }
+
+    public function testCorrectLogin()
+    {
+        $crawler = $this->client->request('GET', '/login');
+
+        $form = $crawler->selectButton('submit')->form();
+        $form['email'] = 'user@user.com';
+        $form['password'] = 'pass';
+        $crawler = $this->client->submit($form);
+
+        $this->assertResponseRedirects('/');
+    }
+
+    public function testIncorrectLogin()
+    {
+        $crawler = $this->client->request('GET', '/login');
+
+        $form = $crawler->selectButton('submit')->form();
+        $form['email'] = 'incorrect@user.com';
+        $form['password'] = 'wrong';
+        $crawler = $this->client->submit($form);
+
+        $this->assertRouteSame('app_login');
+    }
+}
