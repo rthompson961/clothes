4d2d0342d4d8528eaa0d1e8c423dcbebb1fc8ba7
richard <richard@email.com>
2019-12-27 19:37:15 +0000

    Updated checkout tests to require login

--- tests/Controller/CheckoutControllerTest.php
+++ tests/Controller/CheckoutControllerTest.php
@@ -6,12 +6,25 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class CheckoutControllerTest extends WebTestCase
 {
-    public function testShowPost()
+    public function testGuestResponse()
     {
         $client = static::createClient();
 
         $client->request('GET', '/checkout');
+        $this->assertResponseRedirects('/login');
+    }
 
-        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+    public function testUserResponse()
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', '/login');
+        $form = $crawler->selectButton('submit')->form();
+        $form['email'] = 'user@user.com';
+        $form['password'] = 'pass';
+        $crawler = $client->submit($form);
+
+        $client->request('GET', '/checkout');
+        $this->assertResponseIsSuccessful();
     }
 }
