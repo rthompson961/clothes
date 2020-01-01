6e2f5745b11ac05356041c2770404d34c33445e5
richard <richard@email.com>
2020-01-01 20:34:03 +0000

    Updated checkout test to ensure basket not empty

--- tests/Controller/CheckoutControllerTest.php
+++ tests/Controller/CheckoutControllerTest.php
@@ -24,6 +24,13 @@ class CheckoutControllerTest extends WebTestCase
         $form['password'] = 'pass';
         $crawler = $client->submit($form);
 
+        // A product must be added to basket before accessing checkout
+        $crawler = $client->request('GET', '/product/2');
+        $form = $crawler->selectButton('form[submit]')->form();
+        $form['form[product]'] = 6;
+        $crawler = $client->submit($form);
+        $this->assertResponseRedirects('/basket');
+
         $client->request('GET', '/checkout');
         $this->assertResponseIsSuccessful();
     }
