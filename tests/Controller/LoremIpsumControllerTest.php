02e38bde9e314e4b8cabb0a51dbbf0b3b23a19e3
richard <richard@email.com>
2020-03-05 17:26:42 +0000

    Added static text pages

---
+++ tests/Controller/LoremIpsumControllerTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class LoremIpsumControllerTest extends WebTestCase
+{
+
+   /**
+     * @dataProvider pageProvider
+     */    
+    public function testResponse($page)
+    {
+       $client = static::createClient();
+       $client->request('GET', '/' . $page);
+       $this->assertResponseIsSuccessful();
+    }
+
+    public function pageProvider()
+    {
+        return [
+            ['about'],
+            ['privacy'],
+            ['faq'],
+            ['delivery'],
+            ['terms'],
+            ['returns']
+        ];
+    }
+}
