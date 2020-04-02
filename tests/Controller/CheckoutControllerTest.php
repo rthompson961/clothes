0d375e98a8ce8b54e7cbcf875ff06127073ddac2
richard <richard@email.com>
2020-04-02 20:06:21 +0100

    removed checkout test repeated login code

--- tests/Controller/CheckoutControllerTest.php
+++ tests/Controller/CheckoutControllerTest.php
@@ -8,93 +8,71 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 class CheckoutControllerTest extends WebTestCase
 {
     private KernelBrowser $client;
+    private array $sandbox;         
 
     protected function setUp(): void
     {
         $this->client = static::createClient();
+
+        $this->sandbox['card_success'] = '5424000000000015';
+        $this->sandbox['card_failure'] = '5424000000000010';
+        $this->sandbox['expiry']       = '1220';
+        $this->sandbox['cvs']          = '999';
+
+        // login
+        $crawler = $this->client->request('GET', '/login');
+        $form = $crawler->selectButton('submit')->form();
+        $form['email']    = 'user@user.com';
+        $form['password'] = 'pass';
+        $crawler = $this->client->submit($form);
     }
 
     public function testGuestRedirect(): void
     {
+        // destroy session
+        $this->client->restart();
+
         $this->client->request('GET', '/checkout');
         $this->assertResponseRedirects('/login');
     }
 
     public function testNoBasketRedirect(): void
     {
-        // login
-        $crawler = $this->client->request('GET', '/login');
-        $form = $crawler->selectButton('submit')->form();
-        $form['email']    = 'user@user.com';
-        $form['password'] = 'pass';
-        $crawler = $this->client->submit($form);
-
         $this->client->request('GET', '/checkout');
         $this->assertResponseRedirects('/basket');
     }
 
-    public function testCardFailure(): object
+    public function testCardFailure(): void
     {
-        // login
-        $crawler = $this->client->request('GET', '/login');
-        $form = $crawler->selectButton('submit')->form();
-        $form['email']    = 'user@user.com';
-        $form['password'] = 'pass';
-        $crawler = $this->client->submit($form);
-
         // add product
         $crawler = $this->client->request('GET', '/product/1');
         $form = $crawler->selectButton('form[submit]')->form();
         $form['form[product]'] = '1';
         $crawler = $this->client->submit($form);
 
-        // add another product
-        $crawler = $this->client->request('GET', '/product/1');
-        $form = $crawler->selectButton('form[submit]')->form();
-        $form['form[product]'] = '2';
-        $crawler = $this->client->submit($form);
-
-        // checkout with incorrect card number
-        $sandbox['card']   = '5424000000000010';
-        $sandbox['expiry'] = '1220';
-        $sandbox['cvs']    = '999';
-
         $crawler = $this->client->request('GET', '/checkout');
         $form = $crawler->selectButton('form[submit]')->form();
-        $form['form[card]']    = $sandbox['card'];
-        $form['form[expiry]']  = $sandbox['expiry'];
-        $form['form[cvs]']     = $sandbox['cvs'];
+        $form['form[card]']    = $this->sandbox['card_failure'];
+        $form['form[expiry]']  = $this->sandbox['expiry'];
+        $form['form[cvs]']     = $this->sandbox['cvs'];
         $crawler = $this->client->submit($form);
-        $this->assertRouteSame('checkout');
 
-        return $this->client->getContainer()->get('session');
+        $this->assertRouteSame('checkout');
     }
 
-    /**
-     * @depends testCardFailure
-     */
-    public function testCardSuccess(object $session): void
+    public function testCardSuccess(): void
     {
-        $basket = $session->get('basket');
-        $this->client->getContainer()->get('session')->set('basket', $basket);
-
-        // login
-        $crawler = $this->client->request('GET', '/login');
-        $form = $crawler->selectButton('submit')->form();
-        $form['email']    = 'user@user.com';
-        $form['password'] = 'pass';
+        // add product
+        $crawler = $this->client->request('GET', '/product/1');
+        $form = $crawler->selectButton('form[submit]')->form();
+        $form['form[product]'] = '1';
         $crawler = $this->client->submit($form);
-
-        // checkout with incorrect card number
-        $sandbox['card']   = '5424000000000015';
-        $sandbox['expiry'] = '1220';
-        $sandbox['cvs']    = '999';
 
         $crawler = $this->client->request('GET', '/checkout');
         $form = $crawler->selectButton('form[submit]')->form();
-        $form['form[card]']    = $sandbox['card'];
-        $form['form[expiry]']  = $sandbox['expiry'];
-        $form['form[cvs]']     = $sandbox['cvs'];
+        $form['form[card]']    = $this->sandbox['card_success'];
+        $form['form[expiry]']  = $this->sandbox['expiry'];
+        $form['form[cvs]']     = $this->sandbox['cvs'];
         $crawler = $this->client->submit($form);
 
         $this->assertResponseRedirects('/shop');
