57082b13a8f39cfae2425b21eecd18e8a02bf596
richard <richard@email.com>
2020-04-09 13:41:26 +0100

    added register tests

--- tests/Controller/SecurityControllerTest.php
+++ tests/Controller/SecurityControllerTest.php
@@ -14,13 +14,7 @@ class SecurityControllerTest extends WebTestCase
         $this->client = static::createClient();
     }
 
-    public function testResponse(): void
-    {
-        $this->client->request('GET', '/login');
-        $this->assertResponseIsSuccessful();
-    }
-
-    public function testCorrectLogin(): void
+    public function testLoginSuccess(): void
     {
         $crawler = $this->client->request('GET', '/login');
 
@@ -32,7 +26,7 @@ class SecurityControllerTest extends WebTestCase
         $this->assertResponseRedirects('/');
     }
 
-    public function testIncorrectLogin(): void
+    public function testLoginFailue(): void
     {
         $crawler = $this->client->request('GET', '/login');
 
@@ -42,5 +36,73 @@ class SecurityControllerTest extends WebTestCase
         $crawler = $this->client->submit($form);
 
         $this->assertRouteSame('login');
+    }
+
+    public function testRegisterSuccess(): void
+    {
+        $crawler = $this->client->request('GET', '/register');
+
+        $form = $crawler->selectButton('register[submit]')->form();
+        $form['register[email]'] = 'user@gmail.com';
+        $form['register[password]'] = 'password';
+        $crawler = $this->client->submit($form);
+
+        $this->assertResponseRedirects('/');
+    }
+
+    public function assertionProvider(): array
+    {
+        return [
+            [
+                '',
+                '',
+                'This value should not be blank.',
+                2
+            ],
+            [
+                'user.gmail.com',
+                'password',
+                'This value is not a valid email address.',
+                 1
+            ],
+            [
+                str_repeat("a", 171) . '@gmail.com',
+                'password',
+                'This value is too long. It should have 180 characters or less.',
+                 1
+            ],
+            [
+                'user@gmail.com',
+                str_repeat("a", 51),
+                'This value is too long. It should have 50 characters or less.',
+                 1
+            ],
+            [
+                'user@gmail.com',
+                'passwor',
+                'This value is too short. It should have 8 characters or more.',
+                 1
+            ],
+        ];
+    }
+
+   /**
+     * @dataProvider assertionProvider
+     */
+    public function testRegisterAssertions(
+        string $email,
+        string $pass,
+        string $error,
+        int    $count
+    ): void {
+        $crawler = $this->client->request('GET', '/register');
+
+        $form = $crawler->selectButton('register[submit]')->form();
+        $form['register[email]'] = $email;
+        $form['register[password]'] = $pass;
+        $crawler = $this->client->submit($form);
+
+        $this->assertSelectorTextSame('#register li', $error);
+        $this->assertEquals($count, $crawler->filter('#register li')->count());
     }
 }
