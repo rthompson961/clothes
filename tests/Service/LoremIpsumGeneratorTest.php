466c26b77a1b6706b2976a4b093714b116543f5b
richard <richard@email.com>
2020-06-03 11:53:48 +0100

    removed rudimentary lorem ipsum service assertions

--- tests/Service/LoremIpsumGeneratorTest.php
+++ tests/Service/LoremIpsumGeneratorTest.php
@@ -10,13 +10,10 @@ class LoremIpsumGeneratorTest extends TestCase
     public function testParagraph(): void
     {
         $ipsum  = new LoremIpsumGenerator();
-        $paragraph = $ipsum->getParagraph(10);
+        $text = $ipsum->getParagraph(10);
 
         // no duplicates
-        $array = explode(' ', $paragraph);
-        $this->assertTrue(count($array) === count(array_unique($array)));
-        // starts with a capital letter and ends with fullstop
-        $this->assertTrue(ctype_upper(substr($paragraph, 0, 1)));
-        $this->assertStringEndsWith('.', $paragraph);
+        $text = explode(' ', $text);
+        $this->assertTrue(count($text) === count(array_unique($text)));
     }
 }
