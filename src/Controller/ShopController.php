4d7ff23de33b7fd6f5486d15af16845ddbb53f1e
richard <you@example.com>
2020-08-28 22:00:07 +0100

    removed query string sanitiser service

--- src/Controller/ShopController.php
+++ src/Controller/ShopController.php
@@ -7,8 +7,8 @@ use App\Entity\Category;
 use App\Entity\Colour;
 use App\Entity\Product;
 use App\Service\ShopBuilder;
-use App\Service\QueryStringSanitiser;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -17,16 +17,37 @@ class ShopController extends AbstractController
     /**
      * @Route("/shop", name="shop")
      */
-    public function index(QueryStringSanitiser $queryString, shopBuilder $builder): Response
+    public function index(Request $request, shopBuilder $builder): Response
     {
         // store requested page number, sort order & filters
-        $query['page'] = $queryString->get('page');
-        $query['sort'] = $queryString->getChoice('sort', ['first', 'name', 'low', 'high']);
-        foreach (['category', 'brand', 'colour'] as $key) {
-            $query['filters'][$key] = $queryString->getList($key);
+        $query['page'] = $request->query->getInt('page');
+        $query['sort'] = $request->query->get('sort');
+        if (!in_array($query['sort'], ['first', 'name', 'low', 'high'])) {
+            $query['sort'] = 'first';
         }
+
+        foreach (['category', 'brand', 'colour'] as $key) {
+            if (is_array($request->query->get($key)) || !$request->query->get($key)) {
+                $result = [];
+            } else {
+                 // split comma separated list into array
+                $result = explode(',', $request->query->get($key));
+                // convert each element of array to positive integer
+                array_walk($result, function (&$val) {
+                    $val = abs((int) $val);
+                });
+                // remove zero value elements
+                $result = array_filter($result);
+            }
+            $query['filters'][$key] = $result;
+        }
+
         $query['limit'] = 6;
         $query['offset'] = $query['page'] * $query['limit'] - $query['limit'];
+
+        if ($query['offset'] < 1) {
+            $query['offset'] = 0;
+        }
 
         // get product count and products for the current page
         $doctrine = $this->getDoctrine();
