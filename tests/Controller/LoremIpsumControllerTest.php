0b4ea7ea3b368b4757693c169f13fb51a18c3af0
richard <you@example.com>
2020-08-30 15:27:53 +0100

    added static page tests

---
+++ tests/Controller/LoremIpsumControllerTest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class LoremIpsumControllerTest extends WebTestCase
+{
+    /**
+     * @dataProvider pageProvider
+     */
+    public function testPages(string $page): void
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/');
+
+        $link = $crawler->filter('#footer a.' . $page)->link();
+        $crawler = $client->click($link);
+
+        $this->assertResponseIsSuccessful();
+    }
+
+    public function pageProvider(): array
+    {
+        return [
+            ['about'],
+            ['privacy'],
+            ['faq'],
+            ['delivery'],
+            ['terms'],
+            ['returns']
+        ];
+    }
+}
