dc70789cc59bbf46940f9aa2739447ab372919f4
richard <you@example.com>
2020-11-11 13:34:06 +0000

    added repositories as shop controller arguments for cleaner doctrine calls

--- src/Controller/ShopController.php
+++ src/Controller/ShopController.php
@@ -2,10 +2,10 @@
 
 namespace App\Controller;
 
-use App\Entity\Brand;
-use App\Entity\Category;
-use App\Entity\Colour;
-use App\Entity\Product;
+use App\Repository\BrandRepository;
+use App\Repository\CategoryRepository;
+use App\Repository\ColourRepository;
+use App\Repository\ProductRepository;
 use App\Service\Shop;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -17,8 +17,14 @@ class ShopController extends AbstractController
     /**
      * @Route("/shop", name="shop")
      */
-    public function index(Request $request, Shop $shop): Response
-    {
+    public function index(
+        Request $request,
+        Shop $shop,
+        BrandRepository $brandRepo,
+        CategoryRepository $categoryRepo,
+        ColourRepository $colourRepo,
+        ProductRepository $productRepo
+    ): Response {
         // store requested product selection filters
         foreach (['category', 'brand', 'colour'] as $key) {
             $filters[$key] = [];
@@ -26,28 +32,25 @@ class ShopController extends AbstractController
                 $filters[$key] = $shop->csvToArray($request->query->get($key));
             }
         }
-
         // store requested sort order and page number
         $sort = $request->query->get('sort', 'first');
         $page = max(1, $request->query->getInt('page'));
 
         // get total product count and product details for the current page
-        $repo = $this->getDoctrine()->getRepository(Product::class);
-        $count = $repo->findProductCount($filters);
-        $products = $repo->findProducts($filters, $sort, $page);
+        $count    = $productRepo->findProductCount($filters);
+        $products = $productRepo->findProducts($filters, $sort, $page);
 
-        // filter options that require links to be generated for them so they can be applied
-        $options['category'] = $this->getDoctrine()->getRepository(Category::class)->findAll();
-        $options['brand']    = $this->getDoctrine()->getRepository(Brand::class)->findAll();
-        $options['colour']   = $this->getDoctrine()->getRepository(Colour::class)->findAll();
-        // links to add/remove categories, brands and colours
+        // potential filter options that the user may want to select
+        $options['category'] = $categoryRepo->findBy([], ['name' => 'ASC']);
+        $options['brand']    = $brandRepo->findBy([], ['name' => 'ASC']);
+        $options['colour']   = $colourRepo->findBy([], ['name' => 'ASC']);
+
+        // navigation links to add/remove category, brandy and colour options
         $links['filters'] = $shop->getFilterLinks($options, $filters, $sort);
-
-        // links to change sort order
+        // navigation links to change sort order
         $links['sort'] = $shop->getSortLinks($filters, $sort);
-
-        // links to change page
-        $lastPage = (int) ceil($count / $repo::ITEMS_PER_PAGE);
+        // navigation links to change page
+        $lastPage = (int) ceil($count / $productRepo::ITEMS_PER_PAGE);
         $links['page'] = $shop->getPageLinks($filters, $sort, $page, $lastPage);
 
         return $this->render('shop/index.html.twig', [
