40b5f55007f119a751f4af555a1f3b541a3a9114
richard <you@example.com>
2020-12-10 15:48:57 +0000

    simplified security controller tests

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -20,16 +20,20 @@ class SecurityController extends AbstractController
      */
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
-        // if ($this->getUser()) {
-        //     return $this->redirectToRoute('target_path');
-        // }
+        // redirect already logged in user
+        if ($this->getUser()) {
+            return $this->redirectToRoute('home');
+        }
 
         // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
         // last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
 
-        return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
+        return $this->render('security/login.html.twig', [
+            'last_username' => $lastUsername,
+            'error' => $error
+        ]);
     }
 
     /**
@@ -37,7 +41,7 @@ class SecurityController extends AbstractController
      */
     public function logout(): void
     {
-        throw new \Exception('This method can be blank - it will be intercepted by the logout key on your firewall');
+        throw new \Exception('Blank method - intercepted by firewall');
     }
 
     /**
@@ -49,11 +53,15 @@ class SecurityController extends AbstractController
         GuardAuthenticatorHandler $guard,
         LoginFormAuthenticator $authenticator
     ): ?Response {
+        // redirect already logged in user
+        if ($this->getUser()) {
+            return $this->redirectToRoute('home');
+        }
+
         $form = $this->createForm(RegisterType::class);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            /** @var User $user */
             $user = $form->getData();
             $user->setPassword($encoder->encodePassword(
                 $user,
--- tests/Controller/SecurityControllerTest.php
+++ tests/Controller/SecurityControllerTest.php
@@ -2,33 +2,32 @@
 
 namespace App\Tests\Controller;
 
-use Symfony\Bundle\FrameworkBundle\KernelBrowser;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class SecurityControllerTest extends WebTestCase
 {
-    private KernelBrowser $client;
-
-    protected function setUp(): void
-    {
-        $this->client = static::createClient();
-    }
-
     public function testLoginSuccess(): void
     {
-        $crawler = $this->client->request('GET', '/login');
-        $crawler = $this->client->submitForm('submit', [
+        $client = static::createClient();
+        $client->request('GET', '/login');
+        $client->submitForm('submit', [
             'email'    => 'user@user.com',
             'password' => 'pass'
         ]);
+
+        $this->assertResponseRedirects('/');
+
+        // test redirect when accessing login as logged in user
+        $client->request('GET', '/login');
 
         $this->assertResponseRedirects('/');
     }
 
     public function testLoginFailure(): void
     {
-        $crawler = $this->client->request('GET', '/login');
-        $crawler = $this->client->submitForm('submit', [
+        $client = static::createClient();
+        $client->request('GET', '/login');
+        $client->submitForm('submit', [
             'email'    => 'incorrect@user.com',
             'password' => 'wrong'
         ]);
@@ -38,11 +37,17 @@ class SecurityControllerTest extends WebTestCase
 
     public function testRegisterSuccess(): void
     {
-        $crawler = $this->client->request('GET', '/register');
-        $crawler = $this->client->submitForm('register[submit]', [
+        $client = static::createClient();
+        $client->request('GET', '/register');
+        $client->submitForm('register[submit]', [
             'register[email]'    => 'user@gmail.com',
             'register[password]' => 'password'
         ]);
+
+        $this->assertResponseRedirects('/');
+
+        // test redirect when accessing register as logged in user
+        $client->request('GET', '/register');
 
         $this->assertResponseRedirects('/');
     }
@@ -52,8 +57,9 @@ class SecurityControllerTest extends WebTestCase
      */
     public function testRegisterValidation(string $email, string $pass, string $error): void
     {
-        $crawler = $this->client->request('GET', '/register');
-        $crawler = $this->client->submitForm('register[submit]', [
+        $client = static::createClient();
+        $client->request('GET', '/register');
+        $client->submitForm('register[submit]', [
             'register[email]'    => $email,
             'register[password]' => $pass
         ]);
@@ -63,36 +69,38 @@ class SecurityControllerTest extends WebTestCase
 
     public function validationProvider(): array
     {
-        $max['email'] = 180;
-        $max['pass']  = 50;
-        $domain = '@gmail.com';
+        $validEmail = 'user@gmail.com';
+        $validPass  = 'password';
+        // max email length is 180, 171 + 10 domain chars is 181
+        $longEmail = str_repeat('a', 171) . '@gmail.com';
+        $longPass  = str_repeat('a', 51);
 
         return [
-            [
+            'Blank form' => [
                 '',
                 '',
                 'This value should not be blank.'
             ],
-            [
+            'Email invalid' => [
                 'user.gmail.com',
-                'password',
+                $validPass,
                 'This value is not a valid email address.'
             ],
-            [
-                str_repeat('a', $max['email'] + 1 - strlen($domain)) . $domain,
-                'password',
+            'Email too long' => [
+                $longEmail,
+                $validPass,
                 'This value is too long. It should have 180 characters or less.'
             ],
-            [
-                'user@gmail.com',
-                str_repeat('a', $max['pass'] + 1),
-                'This value is too long. It should have 50 characters or less.'
-            ],
-            [
-                'user@gmail.com',
+            'Password too short' => [
+                $validEmail,
                 'passwor',
                 'This value is too short. It should have 8 characters or more.'
             ],
+            'Password too long' => [
+                $validEmail,
+                $longPass,
+                'This value is too long. It should have 50 characters or less.'
+            ]
         ];
     }
 }
